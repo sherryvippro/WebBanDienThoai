6f14803a1a8791ea25d45d67ad898e4daab75dec
sherryvippro <sherryvippro@gmail.com>
2023-11-03 22:48:13 +0700

    login

---
+++ WebBanDienThoai/Models/ViewModels/SignupViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBanDienThoai.Models.ViewModels
+{
+    public class SignupViewModel
+    {
+        [Required(ErrorMessage = "Không được để trống")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        public string passWord { get; set; }
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [Compare("passWord", ErrorMessage = "Mật khẩu không khớp")]
+        public string confirmPassword { get; set; }
+        [Required(ErrorMessage = "Email không được để trống")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string email { get; set; }
+    }
+}
